f33ef43bfafd5b9ae8ca54cddb031e094cb20456
Nurgali <nrknrps@gmail.com>
2024-07-02 17:22:16 +0500

    dark mode fixes

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,5 +1,7 @@
+import 'package:first_app/themes/theme_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -28,8 +30,11 @@ class SettingsPage extends StatelessWidget {
             const Text("Dark Mode"),
             //switch toggle
             CupertinoSwitch(
-              value: false,
-              onChanged: (value) {},
+              value:
+                  Provider.of<ThemeProvider>(context, listen: false).isDarkMode,
+              onChanged: (value) =>
+                  Provider.of<ThemeProvider>(context, listen: false)
+                      .toggleTheme(),
             ),
           ],
         ),
